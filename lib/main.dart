378bea6aaa9ae841f11e11871ba93ba834c0a2c9
Santiago Puentes <santiago.puentesg@utadeo.edu.co>
2023-09-07 23:49:40 -0500

    Ahora si

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,71 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+void main() {
+  runApp(ProfileCardApp());
+}
 
+class ProfileCardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Perfil de usuario'),
+          title: Text('Profile Card Example'),
         ),
-        body: Container(
-          child: Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://upload.wikimedia.org/wikipedia/commons/5/52/Spider-Man.jpg'),
-                  child: const Text('Santiago Puentes'),
-                )
-              ],
+        body: Center(
+          child: ProfileCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.red,
+      ),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/5/52/Spider-Man.jpg', // URL de tu imagen de perfil
             ),
           ),
-        ),
+          SizedBox(height: 16),
+          Text(
+            'Santiago Puentes Garcia',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey, // Color del texto del nombre
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Ing. Sistemas',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black, // Color del texto de la descripción
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Sexto Semestre',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
       ),
     );
   }
